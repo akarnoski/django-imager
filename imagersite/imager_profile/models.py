0541fa50742ec325e3aa3b5d624246cae71a17ad
Mark Reynoso <mreynoso@spu.edu>
2017-11-20 15:00:12 -0800

    created profile class in models

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ImagerProfile(models.Model):
+    """Create a new imager profile."""
+    user = models.OneToOneField(User)
+    phone = models.CharField(max_length=11, blank=False)
+    website = models.URLField()
+    location = models.CharField(max_length=30, blank=False)
+    fee = models.DecimalField(decimal_places=2, max_digits=6, blank=False)
+    CAMERAS = {
+        ('CANON', 'Canon'),
+        ('NIKON', 'Nikon'),
+        ('SONY', 'Sony'),
+        ('FUJIFILM', 'Fujifilm')
+    }
+    camera = models.CharField(
+        max_length=4,
+        choices=CAMERAS,
+        default='CANON'
+    )
+    SERVICES = {
+        ('WEDDINGS', 'Weddings'),
+        ('SCHOOL', 'School'),
+        ('FAMILY', 'Family'),
+        ('BABIES', 'Babies'),
+        ('NATURE', 'Nature'),
+        ('ABSTACT', 'Abstract'),
+        ('OTHER', 'Other')
+    }
+    services = models.CharField(
+        max_length=7,
+        choices=SERVICES,
+        default='OTHER'
+    )
+    bio = models.TextField()
+    PHOTO_STYLES = {
+        ('BW', 'Black and white'),
+        ('COLOR', 'Color'),
+        ('STILL', 'Still'),
+        ('ACTION', 'Action'),
+    }
+    photo_styles = models.CharField(
+        max_length=4,
+        choices=PHOTO_STYLES,
+        default='COLOR'
+    )

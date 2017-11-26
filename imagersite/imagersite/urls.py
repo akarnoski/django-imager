d1043e50bc0520365f58fd94133f2a2b10d25daf
Mark Reynoso <mreynoso@spu.edu>
2017-11-26 12:57:31 -0800

    added bootstrap

--- imagersite/imagersite/urls.py
+++ imagersite/imagersite/urls.py
@@ -1,4 +1,4 @@
-"""imagersite URL Configuration
+"""imagersite URL Configuration.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
@@ -26,5 +28,4 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^accounts/', include('registration.backends.hmac.urls')),
-
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

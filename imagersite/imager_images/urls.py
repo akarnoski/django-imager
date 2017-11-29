d504cfdd582def39c5d1bb5d5f786b1d45735010
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-11-29 14:13:01 -0800

    couple more changes

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -6,7 +6,7 @@ from imager_images import views
 
 app_name = 'imager_images'
 urlpatterns = [
-    url(r'^upload$', views.upload_view, name='upload_view'),
+    url(r'^photos', views.photo_view, name='photo'),
     url(r'^photos/(?P<number>\d+)', views.photo_view, name='photo'),
     url(r'^album$', views.album_view, name='album'),
     url(r'^library', views.library_view, name='library'),
--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -7,27 +7,6 @@ from imager_images.models import Photo
 from imager_images.forms import DocumentForm, AlbumForm
 
 
-def upload_view(request):
-    """View for displaying the form for users to upload photos."""
-    if request.method == 'POST':
-        form = DocumentForm(request.POST, request.FILES)
-        if form.is_valid():
-            post = form.save()
-            post.user = request.user
-            post.published_date = timezone.now()
-            post.save()
-            return HttpResponseRedirect(reverse('upload_view', current_app='imager_images', args=()))
-    else:
-        form = DocumentForm()
-    documents = Photo.objects.all()
-
-    return render(
-        request,
-        'imager_images/upload.html',
-        context={'documents': documents, 'form': form}
-    )
-
-
 def album_view(request):
     """View to create albums."""
     if request.method == 'POST':
@@ -52,4 +31,7 @@ def library_view(request):
 
 def photo_view(request, number=None):
     """Callable view for the libraaries."""
-    return render(request, 'imager_images/photo.html', context={'photo_id': number})
+    if number is None:
+        return render(request, 'imager_images/photo.html')
+    else:
+        return render(request, 'imager_images/photo.html', context={'photo_id': number})

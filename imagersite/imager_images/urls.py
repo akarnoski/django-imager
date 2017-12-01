ca2585d8f25eae590c3212229afb65a5b8aad5c9
adriennekarnoski <adrienne.j.karnoski@gmail.com>
2017-11-30 18:03:32 -0800

    added form

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -1,13 +1,14 @@
 from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
-from imager_images.views import AlbumView, AlbumPhotoView
+from imager_images.views import AlbumView, AlbumPhotoView, PhotoListView, PhotoCreate
 
 from imager_images import views
-from imager_images.views import PhotoListView
+
 
 app_name = 'imager_images'
 urlpatterns = [
+    url(r'^photos/add', PhotoCreate.as_view(success_url="add"), name='photoupload'),
     url(r'^album$', AlbumView.as_view(), name='album'),
     url(r'^album/(?P<pk>\d+)', AlbumPhotoView.as_view(), name='albumphoto'),
     url(r'^photos', PhotoListView.as_view(), name='photo'),
--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -3,9 +3,10 @@ from django.template import RequestContext
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView
 
 from imager_images.models import Album, ImagerProfile, Photo
-from imager_images.forms import DocumentForm, AlbumForm
+from imager_images.forms import PhotoUploadForm
 
 
 class AlbumView(ListView):
@@ -51,3 +52,15 @@ class PhotoListView(ListView):
         # profile = ImagerProfile.objects.get(user=self.request.user)
         # return Photo.objects.filter(user=profile)
         return Photo.objects.all().filter(published='PUBLIC').order_by('-date_uploaded')
+
+
+class PhotoCreate(CreateView):
+    model = Photo
+    fields = ['docfile', 'title', 'description', 'published', 'date_published']
+    template_name_suffix = '_create_form'
+
+    def form_valid(self, form):
+        form_user = ImagerProfile.objects.get(user=self.request.user)
+        form.instance.user = form_user
+        return super(PhotoCreate, self).form_valid(form)
+

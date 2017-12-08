4e842002e35747a34912738c988c078ec73716fc
Mark Reynoso <mreynoso@spu.edu>
2017-12-07 18:33:14 -0800

    added gmail account to send activation email

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -139,8 +139,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT=587
+EMAIL_HOST_USER = 'imagerproject@gmail.com'
+EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_PASSWORD')
+DEFAULT_EMAIL_FROM = 'imagerproject@gmail.com'
+
 STATIC_URL = '/static/'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'MEDIA')
